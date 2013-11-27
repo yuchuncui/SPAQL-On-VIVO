9206a9a64e7eb13c7f6c0232276ae3fb7b69969b
yuchuncui <cuiyuchun0809@hotmail.com>
2013-11-27 01:09:41 -0500

    Update Publication.java

--- Publication.java
+++ Publication.java
@@ -4,12 +4,30 @@ public class Publication {
 
 	private String title;
 	private String date;
-	private ArrayList<String> authors;
+	private ArrayList<Author> authors;
+	private ArrayList<String> authorNames;
+	private String startPage;
+	private String endPage;
+	private String publishedIn;
+	private String volume;
+	private String DOI;
 
-	public Publication(String title, String date, ArrayList<String> authors) {
+	public Publication(String title, String date, ArrayList<String> authorNames) {
+		this.title = title;
+		this.date = date;
+		this.authorNames = authorNames;
+	}
+
+	public Publication(String title, String date, ArrayList<Author> authors, String startPage, String endPage,
+			String publishedIn, String volume, String DOI) {
 		this.title = title;
 		this.date = date;
 		this.authors = authors;
+		this.startPage = startPage;
+		this.endPage = endPage;
+		this.publishedIn = publishedIn;
+		this.volume = volume;
+		this.DOI = DOI;
 	}
 
 	public String getTitle() {
@@ -20,7 +38,31 @@ public class Publication {
 		return this.date;
 	}
 
-	public ArrayList<String> getAuthors() {
+	public ArrayList<Author> getAuthors() {
 		return this.authors;
+	}
+	
+	public ArrayList<String> getAuthorNames() {
+		return this.authorNames;
+	}
+	
+	public String getEndPage() {
+		return this.endPage;
+	}
+	
+	public String getStartPage() {
+		return this.startPage;
+	}
+	
+	public String getVolumn() {
+		return this.volume;
+	}
+	
+	public String getDOI() {
+		return this.DOI;
+	}
+	
+	public String getPublishedIn() {
+		return this.publishedIn;
 	}
 }
