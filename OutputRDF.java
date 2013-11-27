226f43aa2064225f95ad4d24babfee4598a4bf39
yuchuncui <cuiyuchun0809@hotmail.com>
2013-11-27 10:44:44 -0500

    Update OutputRDF.java

--- OutputRDF.java
+++ OutputRDF.java
@@ -9,10 +9,11 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.sparql.pfunction.library.seq;
 
 public class OutputRDF {
 
-	public void printPubRDF(Publication pub, String sunyreachPubCode) {
+	public static void printPubRDF(Publication pub, String sunyreachPubCode, String format) {
 		Model model = ModelFactory.createDefaultModel();
 		String geo = "http://aims.fao.org/aos/geopolitical.owl#";
 		model.setNsPrefix("geo", geo);
@@ -121,8 +122,92 @@ public class OutputRDF {
 
 		try {
 			OutputStream os = null;
-			os = new FileOutputStream(sunyreachPubCode + ".rdf");
-			model.write(os);
+			if (format.equals("rdf")) {
+				os = new FileOutputStream(sunyreachPubCode + ".rdf");
+				model.write(os);
+				System.out.println("File " + sunyreachPubCode + ".rdf generated.");
+			} else if (format.equals("nt")) {
+				os = new FileOutputStream(sunyreachPubCode + ".nt");
+				model.write(os, "N-TRIPLE");
+				System.out.println("File " + sunyreachPubCode + ".nt generated.");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void addResearcherRDF(Author author, Publication pub, String sunyreachPubCode, String format) {
+		Model model = ModelFactory.createDefaultModel();
+		String geo = "http://aims.fao.org/aos/geopolitical.owl#";
+		model.setNsPrefix("geo", geo);
+		String c4o = "http://purl.org/spar/c4o/";
+		model.setNsPrefix("c4o", c4o);
+		String vitro_public = "http://vitro.mannlib.cornell.edu/ns/vitro/public#";
+		model.setNsPrefix("vitro-public", vitro_public);
+		String skos = "http://www.w3.org/2004/02/skos/core#";
+		model.setNsPrefix("skos", skos);
+		String ero = "http://purl.obolibrary.org/obo/";
+		model.setNsPrefix("ero", ero);
+		String event = "http://purl.org/NET/c4dm/event.owl#";
+		model.setNsPrefix("event", event);
+		String pvs = "http://vivoweb.org/ontology/provenance-support#";
+		model.setNsPrefix("pvs", pvs);
+		String dcelem = "http://purl.org/dc/elements/1.1/";
+		model.setNsPrefix("dcelem", dcelem);
+		String vivo = "http://vivoweb.org/ontology/core#";
+		model.setNsPrefix("vivo", vivo);
+		String vitro = "http://vitro.mannlib.cornell.edu/ns/vitro/0.7#";
+		model.setNsPrefix("vitro", vitro);
+		String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+		model.setNsPrefix("rdf", rdf);
+		String bibo = "http://purl.org/ontology/bibo/";
+		model.setNsPrefix("bibo", bibo);
+		String foaf = "http://xmlns.com/foaf/0.1/";
+		model.setNsPrefix("foaf", foaf);
+		String reach = "http://reach.suny.edu/ontology/core#";
+		model.setNsPrefix("reach", reach);
+		String owl = "http://www.w3.org/2002/07/owl#";
+		model.setNsPrefix("owl", owl);
+		String dcterms = "http://purl.org/dc/terms/";
+		model.setNsPrefix("dcterms", dcterms);
+		String scires = "http://vivoweb.org/ontology/scientific-research#";
+		model.setNsPrefix("scires", scires);
+		String fabio = "http://purl.org/spar/fabio/";
+		model.setNsPrefix("fabio", fabio);
+		String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
+		model.setNsPrefix("rdfs", rdfs);
+
+		Property type = model.createProperty(rdf + "type");
+		Property label = model.createProperty(rdfs + "label");
+		Property linkedAuthor = model.createProperty(vivo + "linkedAuthor");
+		Property linkedInformationResource = model.createProperty(vivo + "linkedInformationResource");
+
+		Resource root = model.createResource("http://reach.suny.edu/individual/g" + author.lastName + "_"
+				+ author.firstName + "_" + sunyreachPubCode.substring(1));
+
+		root.addProperty(type, model.createResource("http://vivoweb.org/ontology/core#Relationship"))
+				.addProperty(type, model.createResource("http://vivoweb.org/ontology/core#Authorship"))
+				.addProperty(type, model.createResource("http://www.w3.org/2002/07/owl#Thing"))
+				.addProperty(
+						linkedAuthor,
+						model.createResource("http://reach.suny.edu/individual/" + author.lastName + "_"
+								+ author.firstName))
+				.addProperty(
+						linkedInformationResource,
+						model.createResource("http://reach.suny.edu/individual/" + sunyreachPubCode).addProperty(label,
+								pub.getTitle()));
+		
+		try {
+			OutputStream os = null;
+			if (format.equals("rdf")) {
+				os = new FileOutputStream(author.lastName + "_" + author.firstName + "_add.rdf");
+				model.write(os);
+				System.out.println("File " + author.lastName + "_" + author.firstName + "_add.rdf generated");
+			} else if (format.equals("nt")) {
+				os = new FileOutputStream(author.lastName + "_" + author.firstName + "_add.nt");
+				model.write(os, "N-TRIPLE");
+				System.out.println("File " + author.lastName + "_" + author.firstName + "_add.nt generated");
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -133,14 +218,16 @@ public class OutputRDF {
 		String firstName = "Janos";
 		String lastName = "Hajagos";
 		String middleName = "";
-		Author a = new Author(firstName, lastName, middleName, QueryUtils.isSunyReachMember(firstName, lastName));
+		Author a = new Author(lastName, firstName, middleName, QueryUtils.isSunyReachMember(firstName, lastName));
 		authors.add(a);
 		String title = "Emerging practices for mapping and linking life sciences data using RDF - A case";
 		String publishedIn = "Journal of Web Semantics";
 		String DOI = "10.1016/j.websem.2012.02.003";
 		Publication p = new Publication(title, "2012", authors, "2", "13", publishedIn, "14", DOI);
 
-		new OutputRDF().printPubRDF(p, "a123456788");
-
+		OutputRDF.printPubRDF(p, "a123456788", "rdf");
+		OutputRDF.printPubRDF(p, "a123456788", "nt");
+		OutputRDF.addResearcherRDF(a, p, "a123456788", "rdf");
+		OutputRDF.addResearcherRDF(a, p, "a123456788", "nt");
 	}
 }
